ec7ce8bf8e499ef11d21666736191547a4b1d4f1
chsuong <csuong@ucsd.edu>
2019-08-18 18:02:07 -0400

    added encoding option

--- rosetta/cmdutils/files_to_vw.py
+++ rosetta/cmdutils/files_to_vw.py
@@ -133,7 +133,7 @@ def _tokenize_one(tokenizer, formatter, doc_id_level, path):
     """
     # If path comes from find (and a pipe to stdin), there will be newlines.
     path = path.strip()
-    with open(path, 'r') as f:
+    with open(path, encoding="utf8", errors='ignore') as f:
             text = f.read()
 
     feature_values = tokenizer.text_to_counter(text)
