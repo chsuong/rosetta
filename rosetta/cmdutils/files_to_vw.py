e3cf997ceec62606fb1db375a31afa5f9659f204
chsuong <csuong@ucsd.edu>
2019-08-18 17:29:45 -0400

    added txt file-only condition

--- rosetta/cmdutils/files_to_vw.py
+++ rosetta/cmdutils/files_to_vw.py
@@ -133,7 +133,7 @@ def _tokenize_one(tokenizer, formatter, doc_id_level, path):
     """
     # If path comes from find (and a pipe to stdin), there will be newlines.
     path = path.strip()
-    if p.endswith('.txt'):
+    if path.endswith('.txt'): 
         with open(path, 'r') as f:
             text = f.read()
 
